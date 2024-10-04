28cbad8827718f626331af38425530aed84d3e96
Samantha <112858152+Samantha-Buijs@users.noreply.github.com>
2024-10-04 14:54:44 +0200

    laravel ui ingezet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
     <!-- Scripts -->
     @viteReactRefresh
     @viteReactRefresh
+    @viteReactRefresh
+    @viteReactRefresh
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
