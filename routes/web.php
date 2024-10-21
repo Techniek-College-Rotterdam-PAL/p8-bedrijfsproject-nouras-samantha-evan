e067174362c0c8fcaea18dd4e3ce9ad78212279f
Samantha <112858152+Samantha-Buijs@users.noreply.github.com>
2024-10-21 18:26:49 +0200

    services.blade.php toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LoginController; 
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('account', function () {
-    return view('account');
-});
+Auth::routes();
 
-Route::get('/Inloggen', function () {
-    return view('auth.login');
-});
-
-
-// Home route
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-// POST route for logout
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-
-Route::post('/profile.edit', [App\Http\Controllers\ProfileController::class, 'edit'])->name('edit');
-
-Auth::routes();
-
-// Registration routes
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
-
-
-// Authenticated routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
-
 
