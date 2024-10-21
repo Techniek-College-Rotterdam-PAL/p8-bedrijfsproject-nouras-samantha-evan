d147e5f21f358b0a2c8dbbda8c929e6f437efc13
Samantha <112858152+Samantha-Buijs@users.noreply.github.com>
2024-10-21 18:26:49 +0200

    services.blade.php toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
+
