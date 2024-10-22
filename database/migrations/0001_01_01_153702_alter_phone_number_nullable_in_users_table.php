dd7312cd6143a507af6dc4bc681790b026d4dd09
evan.c <119853517+PERENSAPP@users.noreply.github.com>
2024-10-22 12:44:42 +0200

    Phone number field fix
    phone number included

--- database/migrations/0001_01_01_153702_alter_phone_number_nullable_in_users_table.php
+++ database/migrations/0001_01_01_153702_alter_phone_number_nullable_in_users_table.php
@@ -1,30 +1,25 @@
 <?php
 
-
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number')->nullable()->change(); // Make phone_number nullable
+            $table->string('phone_number')->nullable(false)->change(); // Make phone_number required again
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number')->nullable(false)->change(); // Revert back to not nullable (if needed)
+            $table->string('phone_number')->nullable()->change(); // Optionally make it nullable again
         });
     }
+
 };
